118473ccc13733218da0fd8b9d3d8b440efe035c
Ryo Rafael <ryorafael18@gmail.com>
2024-03-27 16:06:56 +0100

    intro

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,10 +1,7 @@
-import photo from "./header2.png";
-import Image from "next/image";
-
 const Header = () => {
   return (
     <header>
-      <Image src={photo} alt="inside of the restaurant" />
+      <img src="/header2.png" alt="inside of the restaurant" />
     </header>
   );
 };
--- app/components/Intro.js
+++ app/components/Intro.js
@@ -1,9 +1,24 @@
 const Intro = () => {
-    return (
-        <div>
-            Hello world
-        </div>
-    );
-}
+  return (
+    <>
+      <div className="center">
+        <p>established in 1996</p>
+      </div>
+      <div>
+        <h1>Bienvenue à Le Fou Frog</h1>
+      </div>
+      <div>
+        <p>
+          Since 1996, Le Fou Frog has been a bit of France and New York City
+          tucked away on the corner of 5th and Oak in Kansas City's River
+          Market. Chef/Proprietor Mano Rafael and wife, Barbara, invite you to
+          experience fresh and elegant food, soft lighting, cozy tables,
+          charming servers and an atmosphere that is relaxing, fun and totally
+          unpretentious. We invite you to be our guest!
+        </p>
+      </div>
+    </>
+  );
+};
 
 export default Intro;
