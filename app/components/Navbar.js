dbbcebc6a0cb769e175f1c3b8e28b20f77382d35
JinxedQuinn <146730606+JinxedQuinn@users.noreply.github.com>
2024-03-27 15:13:28 +0100

    Added Resa button on navbar

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -25,6 +25,7 @@ const Navbar = () => {
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
+                    <li><a>Home</a></li>
                     <li>
                         <details>
                             <summary>La Carte</summary>
@@ -42,7 +43,7 @@ const Navbar = () => {
                 </ul>
             </div>
             <div className="navbar-end">
-                <a className="btn">Home</a>
+                <a className="btn">Reservation</a>
             </div>
         </div>
     );
