ec8d46eb604db2aea3d321d1b277e47b9a49dd8e
JinxedQuinn <146730606+JinxedQuinn@users.noreply.github.com>
2024-03-27 15:53:44 +0100

    Added Big Part of Carousel

--- app/components/Carousel.js
+++ app/components/Carousel.js
@@ -4,23 +4,17 @@ const Carousel = () => {
             <div className="carousel-item">
                 <img src="/escargot2.png" alt="Escargots" />
             </div>
-            <div className="carousel-item">
-                <img src="/" alt="Burger" />
+            <div className="carousel-item w-96">
+                <img src="/foieGras.jpg" alt="Foie Gras" />
             </div>
             <div className="carousel-item">
-                <img src="/" alt="Burger" />
+                <img src="/steakaupoivrenew.webp" alt="Steak Au Poivre" />
             </div>
             <div className="carousel-item">
-                <img src="/" alt="Burger" />
+                <img src="/profiteroles1.png" alt="Profiteroles" />
             </div>
-            <div className="carousel-item">
-                <img src="/" alt="Burger" />
-            </div>
-            <div className="carousel-item">
-                <img src="/" alt="Burger" />
-            </div>
-            <div className="carousel-item">
-                <img src="/" alt="Burger" />
+            <div className="carousel-item w-96">
+                <img src="/fish.jpeg" alt="Burger" />
             </div>
         </div>
     );
